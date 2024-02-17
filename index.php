4dd13cd30fbe2e042811e715a43f71f3c948899b
Jack21Ace <jack2119hv@gmail.com>
2024-02-16 20:59:58 -0500

    Fristh Task

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Red Social</title>
+</head>
+<body>
+
+    <
+
+    <h1>Login Usuario</h1>
+    <h3>Usuario</h3><input type="text" name="usuario"> <br> <br>
+    <h3>Password</h3><input type="password" name="password"> <br> <br>
+    <input type="submit" value="Login">
+</body>
+</html>
